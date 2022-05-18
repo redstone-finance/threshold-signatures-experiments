ca9aa9b3b55b8e49053cd5492f9d8eaeee31ff7a
Pavel Senchanka <pavel.senchanka@gmail.com>
2022-05-17 21:29:56 -0400

    Finish ECDSA threshold signatures and tests.

--- contracts/ECDSAVerifier.sol
+++ contracts/ECDSAVerifier.sol
@@ -2,16 +2,37 @@
 pragma solidity ^0.8.0;
 
 contract ECDSAVerifier {
-  address private oracle;
+  address[] private oracles;
+  uint threshold;
 
-  constructor(address _oracle) {
-    oracle = _oracle;
+  constructor(address[] memory _oracles, uint _threshold) {
+    oracles = _oracles;
+    threshold = _threshold;
   }
 
-  function verifyEip191Ecdsa(bytes32 hash, uint8 v, bytes32 r, bytes32 s)
-  public returns (bool) {
+  function verify(bytes32 hash,
+                  bool[] memory signers,
+                  uint8[] memory v,
+                  bytes32[] memory r,
+                  bytes32[] memory s)
+  public view returns (bool) {
+    require(oracles.length == signers.length, "signers must have length equal to oracles");
+
     bytes32 messageDigest = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
-    address signer = ecrecover(messageDigest, v, r, s);
-    return signer == oracle;
+
+    uint nextSignatureIndex = 0;
+    for (uint oracleIndex = 0; oracleIndex < signers.length; oracleIndex++) {
+      if (signers[oracleIndex]) {
+        uint i = nextSignatureIndex;
+        address signer = ecrecover(messageDigest, v[i], r[i], s[i]);
+        require(signer == oracles[oracleIndex], "invalid signature");
+        nextSignatureIndex++;
+      }
+    }
+
+    uint numberOfSignatures = nextSignatureIndex;
+    require(numberOfSignatures >= threshold, "too few signers");
+    
+    return true;
   }
 }
--- test/ecdsa-test.js
+++ test/ecdsa-test.js
@@ -2,28 +2,105 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("ECDSAVerifier", function () {
-  it("Should admit correct signatures", async function () {
-    const signers = await hre.ethers.getSigners();
-    // Arbitrary.
-    const oracleSigner = signers[0];
+  const groupSizes = [
+    // M out of N.
+    [ 5, 8 ],
+    [ 9, 16 ],
+    [ 17, 32 ],
+  ];
 
-    const ECDSAVerifier = await hre.ethers.getContractFactory("ECDSAVerifier");
-    const verifier = await ECDSAVerifier.deploy(oracleSigner.address);
-    await verifier.deployed();
+  function randomSigners(length) {
+    let signers = [];
+    for (let i = 0; i < length; i++) {
+      signers.push(hre.ethers.Wallet.createRandom());
+    }
 
+    return signers;
+  }
+
+  function getMessageDigestBytes() {
     const message = "Redstone oracles are awesome!";
-
     const messageDigest = hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes(message));
     const messageDigestBytes = hre.ethers.utils.arrayify(messageDigest);
+    
+    return messageDigestBytes;
+  }
 
-    const signature = await oracleSigner.signMessage(messageDigestBytes);
-    const signatureSplit = hre.ethers.utils.splitSignature(signature);
+  async function getSignaturesAndMask(allSigners, numSignatures, messageDigestBytes) {
+    let signatures = await Promise.all(
+      allSigners.slice(0, numSignatures).map(async signer => {
+        let signature = await signer.signMessage(messageDigestBytes);
+        return hre.ethers.utils.splitSignature(signature);
+      }));
 
-    const ok = await verifier.callStatic.verifyEip191Ecdsa(
-      messageDigestBytes, signatureSplit.v, signatureSplit.r, signatureSplit.s);
+    let vs = signatures.map(s => s.v);
+    let rs = signatures.map(s => s.r);
+    let ss = signatures.map(s => s.s);
 
-    console.log(ok);
+    let mask = allSigners.map((_, index) => index < numSignatures);
 
-    expect(ok).to.be.true;
-  });
+    return [vs, rs, ss, mask];
+  }
+
+  for ([threshold, total] of groupSizes) {
+    it(`Should admit minimal quorum (${threshold} of ${total})`, async function () {
+      const signers = randomSigners(total);
+      const addresses = signers.map(signer => signer.address);
+
+      const ECDSAVerifier = await hre.ethers.getContractFactory("ECDSAVerifier");
+      const verifier = await ECDSAVerifier.deploy(addresses, threshold);
+      await verifier.deployed();
+
+      const messageDigestBytes = getMessageDigestBytes();
+      const [vs, rs, ss, mask] = await getSignaturesAndMask(signers, threshold, messageDigestBytes);
+
+      const ok = await verifier.verify(messageDigestBytes, mask, vs, rs, ss);
+
+      expect(ok).to.be.true;
+    });
+
+    it(`Should not admit signers fewer than quorum (${threshold - 1} < ${threshold} of ${total})`, async function() {
+      const signers = randomSigners(total);
+      const addresses = signers.map(signer => signer.address);
+
+      const ECDSAVerifier = await hre.ethers.getContractFactory("ECDSAVerifier");
+      const verifier = await ECDSAVerifier.deploy(addresses, threshold);
+      await verifier.deployed();
+
+      const messageDigestBytes = getMessageDigestBytes();
+
+      const [vs, rs, ss, mask] = await getSignaturesAndMask(signers, threshold - 1, messageDigestBytes);
+      await expect(verifier.verify(
+        messageDigestBytes, mask, vs, rs, ss)).to.be.revertedWith("too few signers");
+    });
+
+    it(`Should not admit even one invalid signature (${threshold} of ${total})`, async function() {
+      const signers = randomSigners(total);
+      const addresses = signers.map(signer => signer.address);
+
+      const ECDSAVerifier = await hre.ethers.getContractFactory("ECDSAVerifier");
+      const verifier = await ECDSAVerifier.deploy(addresses, threshold);
+      await verifier.deployed();
+
+      const messageDigestBytes = getMessageDigestBytes();
+
+      let signatures = await Promise.all(
+        signers.slice(0, threshold).map(async signer => {
+          let signature = await signer.signMessage(messageDigestBytes);
+          return hre.ethers.utils.splitSignature(signature);
+        }));
+
+      let vs = signatures.map(s => s.v);
+      let rs = signatures.map(s => s.r);
+      let ss = signatures.map(s => s.s);
+
+      // Replace a signature with an invalid one.
+      rs[0] = hre.ethers.utils.hexlify(hre.ethers.utils.randomBytes(32));
+
+      let mask = signers.map((_, index) => index < threshold);
+
+      await expect(verifier.verify(
+        messageDigestBytes, mask, vs, rs, ss)).to.be.revertedWith("invalid signature");
+    });
+  }
 });
